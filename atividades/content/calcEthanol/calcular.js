97d9002650f0bfc389e4f49ad4aaa79c275b91cd
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2023-10-21 10:53:04 -0300

    Update calcConbustivel
    melhorias estéticas

--- atividades/content/calcEthanol/calcular.js
+++ atividades/content/calcEthanol/calcular.js
@@ -5,8 +5,10 @@ function melhor(){
     gasolina= parseFloat(frdados.txgasolina.value.replace(",","."));
     if(etanol < 0.7*gasolina){
         document.getElementById("imagemGas").src="imagens/etanol.png"
+        document.getElementById("imagemGas").title="Etanol e melhor"
     }else{
         document.getElementById("imagemGas").src="imagens/gasolina.jpg"
+        document.getElementById("imagemGas").title="Gasolina e melhor"
     }
 }
 
